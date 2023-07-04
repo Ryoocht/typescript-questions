d8e032b8f9f4b302c9d96d3d91452a6a89c96e7a
Ryochi Tanaka <ryo@ryochis-mbp.home>
2023-07-04 21:44:08 +1000

    first commit

---
+++ src/queestions/question-1.ts
@@ -0,0 +1,13 @@
+interface Resident {
+    id: string
+    name: string
+    speciality: string
+}
+
+/* *
+    * Create Doctor type extends Resident type 
+    * Make speciality field in Resident optional
+    * Add experience field to Doctor type
+*/
+
+// type Doctor = 
